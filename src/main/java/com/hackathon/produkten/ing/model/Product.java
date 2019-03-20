63bd85f095d6c60467317bca98616f5d6f4c24a6
prakashsubramanii <prakashooty16.com>
2019-03-21 02:19:52 +0530

    entity commit

--- src/main/java/com/hackathon/produkten/ing/model/Product.java
+++ src/main/java/com/hackathon/produkten/ing/model/Product.java
@@ -1,8 +1,30 @@
 package com.hackathon.produkten.ing.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
-public class Product {
-
+public class Product implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long productId;
+	public Long getProductId() {
+		return productId;
+	}
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	private String productName;
 }
--- src/main/java/com/hackathon/produkten/ing/model/ProductGroup.java
+++ src/main/java/com/hackathon/produkten/ing/model/ProductGroup.java
@@ -1,8 +1,37 @@
 package com.hackathon.produkten.ing.model;
 
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 @Entity
 public class ProductGroup {
-
+    @Id
+	private Long id;
+	private String productName;
+	@OneToMany(cascade=CascadeType.ALL,orphanRemoval=true)
+	@JoinColumn(name="product_id")
+	private List<Product> product;
+	
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public List<Product> getProduct() {
+		return product;
+	}
+	public void setProduct(List<Product> product) {
+		this.product = product;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
